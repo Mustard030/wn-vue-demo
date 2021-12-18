6e8ab81cdde7438f874e6dd910babe0b6664bf19
jiemoo <978090944@qq.com>
2021-12-19 02:39:33 +0800

    Changes

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ axios.interceptors.response.use(config => {
 // axios挂载到this.$http中
 Vue.prototype.$http = axios
 // 默认后端地址
-axios.defaults.baseURL = 'http://127.0.0.1:8090/'
+axios.defaults.baseURL = 'http://localhost:8090/'
 
 new Vue({
   router,
